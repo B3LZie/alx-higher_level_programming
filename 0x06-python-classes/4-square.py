223b1f6e9e7cb1db834cb05d0a22f22c0ba72e9c
B3LZie <belindapondayi@live.com>
2023-01-31 05:10:16 -0800

    Task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,3 +11,36 @@ class Square:
         size (int): The size of the new square.
         """
         self.__size = size
+
+    # Property
+    @property
+    def size(self):
+    """int: private size.
+
+    Returns:
+        Private size.
+    """
+        return self.__size
+
+    # Setter modifies
+    @size.setter
+    def size(self, value):
+        """Sets value into size, must be int.
+
+        Args:
+            value (int): size of the square.
+        """
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """returns the area
+
+        Returns:
+            area.
+        """
+        return self.__size**2
