c09652ba756b105ae74f116293d02d2fb97044dd
B3LZie <belindapondayi@live.com>
2023-01-31 04:52:58 -0800

    Task 103

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
+""" class Square that defines a square"""
+
+
 import math
 
 
 class MagicClass:
+    """ class Square that defines a square"""
     def __init__(self, radius=0):
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
