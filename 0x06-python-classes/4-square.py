3d3348542b5a429717b923b0a3d2104c2b039c44
B3LZie <belindapondayi@live.com>
2023-01-31 04:58:39 -0800

    Tas k4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,44 +1,13 @@
 #!/usr/bin/python3
-""" class Square that defines a square"""
+"""Define a class Square."""
 
 
 class Square:
-    """ class Square that defines a square"""
-    def __init__(self, size=0):
-        """ init square
+    """Represent a square."""
 
+    def __init__(self, size):
+        """Initialize a new Square.
         Args:
-        value (int): size of the square.
+        size (int): The size of the new square.
         """
-        self.size = size
-
-   @property
-   def size(self):
-        """int: private size.
-
-        Returns:
-        Private size.
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Sets value into size, must be int.
-
-        Args:
-        value (int): size of the square.
-        """
-        if type(value) is not int:
-            raise TypeError('size must be an integer')
-        elif value < 0:
-            raise ValueError('size must be >= 0')
-        else:
-            self.__size = value  #: size of the square
-
-    def area(self):
-        """returns the area
-
-        Returns:
-        area.
-        """
-        return self.__size**2
+        self.__size = size
